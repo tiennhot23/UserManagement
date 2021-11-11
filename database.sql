635b27fa585572ba0ef2e4dc88717666b3107641
tiennhot23@gmail.com <tiennhot23@gmail.com>
2021-11-11 11:07:42 +0700

    .

--- database.sql
+++ database.sql
@@ -1,5 +1,12 @@
+CREATE DATABASE usermanagement;
+
+USE usermanagement;
+
+CREATE TABLE `usermanagement`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'active' , `password` VARCHAR(200), PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
 INSERT INTO `user` 
 (`id`, `first_name`,  `last_name`,    `email`,                 `phone`,         `comments`, `status`, `password`) VALUES
+(NULL, 'sa',		'sa ',		'sa@gmail.com',		'0103873948', '',	'active', '$2b$10$Nvo.1E/DoE2yFiTIrxDJq.ZyaW4pErq3w9SERqpwgEo9jeHKyuqzW'),
 (NULL, 'Amanda',      'Nunes',        'anunes@ufc.com',        '012345 678910', '',          'active', '123'),
 (NULL, 'Alexander',   'Volkanovski',  'avolkanovski@ufc.com',  '012345 678910', '',          'active', '123'),
 (NULL, 'Khabib',      'Nurmagomedov', 'knurmagomedov@ufc.com', '012345 678910', '',          'active', '123'),
@@ -17,5 +24,4 @@ INSERT INTO `user`
 (NULL, 'Tecia',       'Torres ',      'ttorres@ufc.com',       '012345 678910', '',          'active', '123'),
 (NULL, 'Ronda',       'Rousey ',      'rrousey@ufc.com',       '012345 678910', '',          'active', '123'),
 (NULL, 'Holly',       'Holm ',        'hholm@ufc.com',         '012345 678910', '',          'active', '123'),
-(NULL, 'Joanna',      'Jedrzejczyk ', 'jjedrzejczyk@ufc.com',  '012345 678910', '',          'active', '123'),
-(NULL, 'sa',			'sa ',			'sa@gmail.com',			'0103873948', '',				'active', '$2b$10$Nvo.1E/DoE2yFiTIrxDJq.ZyaW4pErq3w9SERqpwgEo9jeHKyuqzW')
+(NULL, 'Joanna',      'Jedrzejczyk ', 'jjedrzejczyk@ufc.com',  '012345 678910', '',          'active', '123')
--- user-schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE usermanagement;
-
-USE usermanagement;
-
-CREATE TABLE `usermanagement`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'active' , `password` VARCHAR(200), PRIMARY KEY (`id`)) ENGINE = InnoDB;
